0fc05cf15907c57e79c9029d6f4f5c6bcad87176
Chanjin Park <parkchamchi@gmail.com>
2024-05-27 16:44:06 +0900

    new list endpoints: `corpuses/`, `lists/`

--- src/backend/upload/views.py
+++ src/backend/upload/views.py
@@ -191,8 +191,32 @@ class CorpusesAPIView(APIView):
 			
 			return Response(
 				{
+					"corpus_id": corpus_id,
 					"corpuses_history": corpuses_history,
 				},
+				status=status.HTTP_200_OK
+			)
+	
+		except Exception as e:
+			return Response(
+				{"error": str(e)},
+				status=status.HTTP_400_BAD_REQUEST
+			)
+		
+class CorpusesListAPIView(APIView):
+	parser_classes = [JSONParser]
+	permission_classes = (AllowAny, )
+
+	def get(self, request, *args, **kwargs):
+		try:
+			toret= [
+				{"corpus_id": uc.corpus_id, "corpuses_history": uc.corpuses_history["corpuses_history"]}
+				for uc
+				in UploadedCorpus.objects.all()
+			]
+			
+			return Response(
+				toret,
 				status=status.HTTP_200_OK
 			)
 	
@@ -217,6 +241,34 @@ class TasksAPIView(APIView):
 					"target_corpus_id": str(task.target_corpus_id),
 					"status": task.status
 				},
+				status=status.HTTP_200_OK
+			)
+	
+		except Exception as e:
+			return Response(
+				{"error": str(e)},
+				status=status.HTTP_400_BAD_REQUEST
+			)
+		
+class TasksListAPIView(APIView):
+	parser_classes = [JSONParser]
+	permission_classes = (AllowAny, )
+
+	def get(self, request, *args, **kwargs):
+		try:
+			tasks = Task.objects.all()
+			toret = [
+				{
+					"timestamp": task.timestamp,
+					"target_corpus_id": str(task.target_corpus_id),
+					"status": task.status
+				}
+				for task
+				in tasks 
+			]
+			
+			return Response(
+				toret,
 				status=status.HTTP_200_OK
 			)
 	

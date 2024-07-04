849c9df8bcd3e9e1bc56851c283000e659d5c8f6
parkchamchi <parkchamchi@gmail.com>
2024-07-04 17:06:44 +0900

    dj models change

--- src/backend/upload/dj_serializables.py
+++ src/backend/upload/dj_serializables.py
@@ -2,15 +2,19 @@ from django.db import models
 
 import serializables
 
-class DjToken(models.Model):
-	class Meta:
-		unique_together = (("txt", "gloss", "is_delimiter"), )
-
+class CorpusHeader(models.Model):
 	id = models.AutoField(primary_key=True)
 
-	txt = models.TextField()
-	gloss = models.TextField()
-	is_delimiter = models.BooleanField()
+	current_task = models.IntegerField()
+
+class DjCorpus(models.Model):
+	id = models.AutoField(primary_key=True)
+
+	original_text = models.TextField()
+	p_div_locs = models.TextField()
+
+	corpus_header_info = models.ForeignKey(CorpusHeader, on_delete=models.CASCADE)
+	index = models.IntegerField()
 
 class DjParagraph(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -25,31 +29,18 @@ class DjParagraph(models.Model):
 	annotator_info = models.TextField()
 	original_text = models.TextField()
 
-class DjCorpus(models.Model):
-	id = models.AutoField(primary_key=True)
-
-	original_text = models.TextField()
-	p_div_locs = models.TextField()	
-
-class DjTokensInParagraph(models.Model):
-	class Meta:
-		unique_together = (("dj_paragraph_id", "index", "dj_token_id"), )
-
-	id = models.AutoField(primary_key=True)
-	
-	dj_paragraph_id = models.ForeignKey(DjParagraph, on_delete=models.CASCADE)
+	corpus_id = models.ForeignKey(DjCorpus, on_delete=models.CASCADE)
 	index = models.IntegerField()
-	dj_token_id = models.ForeignKey(DjToken, on_delete=models.CASCADE)
 
-class DjParagraphsInCorpus(models.Model):
-	class Meta:
-		unique_together = (("dj_corpus_id", "index", "dj_paragraph_id"), )
-
+class DjToken(models.Model):
 	id = models.AutoField(primary_key=True)
 
-	dj_corpus_id = models.ForeignKey(DjCorpus, on_delete=models.CASCADE)
+	txt = models.TextField()
+	gloss = models.TextField()
+	is_delimiter = models.BooleanField()
+
+	token_id = models.ForeignKey(DjParagraph, on_delete=models.CASCADE)
 	index = models.IntegerField()
-	dj_paragraph_id = models.ForeignKey(DjParagraph, on_delete=models.CASCADE)
 
 class DjParagraphDelimiter(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -57,13 +48,13 @@ class DjParagraphDelimiter(models.Model):
 
 class DjParagraphDelimitersInCorpus(models.Models):
 	class Meta:
-		unique_together = (("dj_corpus_id", "dj_paragraph_delimiter_id"), )
+		unique_together = (("corpus_id", "paragraph_delimiter_id"), )
 
-	dj_corpus_id = models.ForeignKey(DjCorpus, on_delete=models.CASCADE)
-	dj_paragraph_delimiter_id = models.ForeignKey(DjParagraphDelimiter, on_delete=models.CASCADE)
+	corpus_id = models.ForeignKey(DjCorpus, on_delete=models.CASCADE)
+	paragraph_delimiter_id = models.ForeignKey(DjParagraphDelimiter, on_delete=models.CASCADE)
 
-class CorpusHeader(models.Model):
+class TaskInfo(models.Models):
 	id = models.AutoField(primary_key=True)
 
-	current_task = models.IntegerField()
-	
+	timestamp = models.DateTimeField(auto_now_add=True)
+	target_corpus_id = models.ForeignKey(CorpusHeader, on_delete=models.CASCADE)

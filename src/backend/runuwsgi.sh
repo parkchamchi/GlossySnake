9be4ffba445c9ec8f47ab0c5d100d44baa94a097
Chanjin Park <parkchamchi@gmail.com>
2024-09-09 11:40:33 +0900

    backend: nginx

--- src/backend/runuwsgi.sh
+++ src/backend/runuwsgi.sh
@@ -1,1 +1,1 @@
-sudo /srv/GlossySnake/src/backend/.venv/bin/uwsgi -i /srv/GlossySnake/src/backend/.config/glossysnake.ini
+sudo /srv/GlossySnake/src/backend/.venv/bin/uwsgi -i /srv/GlossySnake/src/backend/.config/uwsgi/glossysnake.ini

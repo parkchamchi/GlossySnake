e18f0dec2528313d69d3d88a4cc3b5bd778d1ed6
parkchamchi <parkchamchi@gmail.com>
2024-09-11 16:01:26 +0900

    backend: `SECRET_KEY` to `.env`

--- src/backend/GlossySnakeBE/settings.py
+++ src/backend/GlossySnakeBE/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import dotenv
+
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,11 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=l$t&l_yiiq#%%1u!h*@425snl&#eybe5(b1*k15dt%be+mw68'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG:
+	SECRET_KEY = 'django-insecure-=l$t&l_yiiq#%%1u!h*@425snl&#eybe5(b1*k15dt%be+mw68'
+else:
+	dotenv.load_dotenv()
+	SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+	if not SECRET_KEY:
+		raise RuntimeError("SECRET KEY NOT DETECTED.")
 
 ALLOWED_HOSTS = [
 	"127.0.0.1",

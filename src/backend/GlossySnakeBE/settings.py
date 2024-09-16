4fb29cc9f4c87b653c4454c6461c78edd74c165e
Chanjin Park <parkchamchi@gmail.com>
2024-09-16 11:39:35 +0900

    backend: allow credentials

--- src/backend/GlossySnakeBE/settings.py
+++ src/backend/GlossySnakeBE/settings.py
@@ -98,6 +98,7 @@ CORS_ALLOWED_ORIGINS = (
 	"http://127.0.0.1:56122",
 	"http://127.0.0.1:56123",
 )
+CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:56122"]
 
 ROOT_URLCONF = 'GlossySnakeBE.urls'

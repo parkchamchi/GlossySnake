a97335f4911fd89ccb6127ccd3ebd08e4078aca1
Chanjin Park <parkchamchi@gmail.com>
2024-09-17 23:04:55 +0900

    adding load static on the top of vue

--- src/backend/buildvue.sh
+++ src/backend/buildvue.sh
@@ -2,6 +2,10 @@
 cd gs_index
 npm run build
 mv dist/index.html dist/vue.html
+
+# Add the line "{% load static %}" at the start of dist/vue.html
+sed -i '1s/^/{% load static %}\n/' dist/vue.html
+
 cd ..
 cp gs_index/dist/vue.html templates/
 cp gs_index/dist/static/* static/

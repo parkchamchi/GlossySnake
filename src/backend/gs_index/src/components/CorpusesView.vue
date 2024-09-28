1c9c84880a66c827d0a349e85efb0f66768f7c27
parkchamchi <parkchamchi@gmail.com>
2024-09-28 17:40:19 +0900

    gs_index: samples

--- src/backend/gs_index/src/components/CorpusesView.vue
+++ src/backend/gs_index/src/components/CorpusesView.vue
@@ -11,9 +11,12 @@
 		data() {
 			return {
 				api: new GsApi(),
+				sampleHost: "https://parkchamchi.github.io/GlossySnake/samples/v1/",
 
 				localCorpuses: [...sampleCorpuses],
 				remoteCorpuses: [],
+
+				remoteSampleFilenames: [],
 			};
 		},
 		methods: {
@@ -22,9 +25,31 @@
 				const data = await res.json();
 				this.remoteCorpuses = data;
 			},
+			async getRemoteSamples() {
+				await fetch(this.sampleHost)
+					.then((data) => data.json())
+					.then((json) => {
+						this.remoteSampleFilenames = json.filenames;
+					});
+			},
+			async getRemoteSampleCorpus(filename) {
+				fetch(this.sampleHost+filename)
+					.then((data) => data.json())
+					.then((json) => {
+						this.localCorpuses.push({
+							"corpus_id": filename.replace(".corpus.json", ""),
+							"corpuses_history": [json],
+						});
+					})
+					.then(() => {
+						this.remoteSampleFilenames.pop(filename);
+					});
+			},
 		},
 		created() {
 			EventBus.on("updateCorpuses", this.updateCorpuses); // Listen for the error event
+
+			this.getRemoteSamples();
 		},
 		beforeDestroy() {
 			EventBus.off("updateCorpuses", this.updateCorpuses); // Clean up the event listener
@@ -33,6 +58,15 @@
 </script>
 
 <template>
+	<div v-if="remoteSampleFilenames.length > 0">
+		<h4>Get a sample:</h4>
+		<p v-for="(filename, index) in remoteSampleFilenames"
+			class="sampleLinkP"
+			@click="getRemoteSampleCorpus(filename)">
+			{{ filename }}
+		</p>
+	</div>
+
 	<div>
 		<Corpus v-for="(corpus, index) in localCorpuses"
 			:key="index"
@@ -46,3 +80,17 @@
 			:remote=true />
 	</div>
 </template>
+
+<style scoped>
+	.sampleLinkP {
+		font-style: italic;
+		text-decoration: underline;
+		cursor: pointer;
+	}
+
+	.disabled{
+		color: grey;
+		text-decoration: none;
+		cursor: default;
+	}
+</style>

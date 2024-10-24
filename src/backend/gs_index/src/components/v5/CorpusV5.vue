e656642fda9a472ea57f4b827e2a92a390c73fc6
Chanjin Park <parkchamchi@gmail.com>
2024-10-24 15:56:34 +0900

    v5: for sharedState

--- src/backend/gs_index/src/components/v5/CorpusV5.vue
+++ src/backend/gs_index/src/components/v5/CorpusV5.vue
@@ -1,5 +1,6 @@
 <script>
 	import sampleCorpusesV5 from "./sampleCorpusesV5.json";
+	import { sharedState } from "../../sharedState.js";
 
 	export default {
 		props: {
@@ -19,16 +20,37 @@
 				const htmlCollection = xmlDoc.getElementsByTagName("html");
 
 				// Convert HTMLCollection to string
-                let htmlString = "";
-                if (htmlCollection.length > 0)
-                    htmlString = htmlCollection[0].innerHTML;
-                
-                return htmlString;
+				let htmlString = "";
+				if (htmlCollection.length > 0)
+					htmlString = htmlCollection[0].innerHTML;
+				
+				return htmlString;
+			},
+
+			glossColor() {
+				return sharedState.glossColor;
+			},
+			glossSize() {
+				return sharedState.glossSize + "%";
+			},
+			txtSize() {
+				return sharedState.txtSize + "%";
 			},
 		}
 	}
 </script>
 
 <template>
-	<div v-html="corpusHtml"></div>
+	<div v-html="corpusHtml" class="corpusHtml"
+		:style="{ '--gloss-color': glossColor, '--gloss-size': glossSize, '--txt-size': txtSize }">
+	</div>
+
 </template>
+
+<style scoped>
+	.corpusHtml {
+		font-size: var(--txt-size);
+		font-family: serif;
+		line-height: calc(var(--txt-size) + var(--gloss-size));
+	}
+</style>
